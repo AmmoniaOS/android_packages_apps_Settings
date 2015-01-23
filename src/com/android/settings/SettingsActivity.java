4f052f941a5e1ff8645bfd1bfe7a55447713ac8e
karon <kameron19921016@yahoo.com>
2015-01-23 13:59:34 +0800

    settings:battry save to smarter
    Change-Id: Ie8df76fc1d29398d390d5ba435794eb9cf1cc04c

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -246,7 +246,7 @@ public class SettingsActivity extends Activity
             R.id.home_settings,
             R.id.dashboard,
             R.id.privacy_settings_cyanogenmod,
-            R.id.battery_save_settings
+            R.id.smarter_control_settings
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
